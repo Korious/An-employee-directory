74e13235f085c84b74235cb364f83f6d5b936961
Noah Olatoye <noaholatoye@gmail.com>
2021-05-14 15:34:38 +0100

    Optimised JS file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,18 @@
-const employee = document.querySelectorAll('.employee');
+const employee = document.querySelectorAll('.employee')
 
 const displayEmployees = (values) =>{
-    
-    employee.forEach(element => {
+    employee.forEach(element =>{
         element.style.display = "none"
         const name = element.children[1].innerHTML.toUpperCase();
         const position = element.children[2].innerHTML.toUpperCase();
         const job_type = element.children[3].innerHTML.toUpperCase();
 
-        if (name.includes(values) === true || position.includes(values) === true || job_type.includes(values) === true) {
-            
+        if (name.includes(values) || position.includes(values) || job_type.includes(values)){
             element.style.display = "block"
         }
-        else if (values === "") {
-            console.log(searchEmployee.value)
-            employees.style.display = "block"
-            console.log(name)
-    
-        }
-    });
-    
+    })
 }
 
 searchEmployee.addEventListener('input', (e) =>{
     displayEmployees(e.target.value.toUpperCase())
 })
-
-
-
